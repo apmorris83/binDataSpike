f9b21b369990dda456f37d1286a3cceb9c633d74
Sarah Jane Morris <apmorris83@gmail.com>
2017-04-19 11:45:20 +0100

    added config file

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  DB: {
+    test: 'mongodb://localhost/northcoders-news-api-test',
+    dev: 'mongodb://user:password@ds163940.mlab.com:63940/bins'
+  },
+  PORT: {
+    test: 3090,
+    dev: 3000
+  }
+};
--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+if (!process.env.NODE_ENV) process.env.NODE_ENV = 'dev';
+
 const express = require ('express');
 const app = express();
 const apiRouter = require('./routes/api');
 const mongoose = require('mongoose');
+var config = require('./config');
+var db = 'mongodb://user:password@ds163940.mlab.com:63940/bins';
+var PORT = config.PORT[process.env.NODE_ENV] || process.env.PORT;
 
-// mongoose.connect('mongodb://localhost/bin_api');
-mongoose.connect('mongodb://user:password@ds163940.mlab.com:63940/bins');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(db, function (err) {
+  if (!err) {
+    console.log(`connected to the Database: ${db}`);
+  } else {
+    console.log(`error connecting to the Database ${err}`);
+  }
+});
 
 app.use(function (request, response, next) {
     console.log('Received request!');
@@ -17,9 +29,9 @@ app.use('/*', function (request, response) {
     response.status(404).send({reason: '404 -ROUTE NOT FOUND'});
 });
 
-app.listen(process.env.PORT || 3000, function (error) {
+app.listen(PORT, function (error) {
     if (error) {
         return console.log(error);
     }
-    console.log('App listening on port 3000');
+    console.log(`listening on port ${PORT}`);
 });
